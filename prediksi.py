56b212c1721293978d3438d5359c11cca917b4e0
zeerafle <vsefareez@gmail.com>
2022-09-27 20:45:35 +0800

    use file picker instead of hard-coded file path

--- prediksi.py
+++ prediksi.py
@@ -2,38 +2,44 @@ from fitur_ekstrak import preprocess
 import joblib
 import cv2
 import os
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
 
+THICKNESS = 2
 PATH_MODEL = os.path.join('output', 'rfc_model.sav')
+
+Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+# show an "Open" dialog box and return the path to the selected file
+filename = askopenfilename()
+print(f"File path: \n{filename}")
 
 model = joblib.load(PATH_MODEL)
 
-PATH = 'dataset\\train\\cordana\\1615455264278.jpg'
-label_name = PATH.split('\\')[-2]
-image_feature = preprocess(PATH, show=True).reshape(1, -1)
+# filename = 'dataset\\train\\pestalotiopsis\\IMG_20210309_123810.jpg'
+label_name = filename.split('/')[-2]
+predicted_label_name = {0: 'cordana',
+                        1: 'healthy',
+                        2: 'pestalotiopsis',
+                        3: 'sigatoka'}
+image_feature = preprocess(filename, show=True).reshape(1, -1)
 
-predicted_label = model.predict(image_feature)
+# predict gambar
+predicted_label = model.predict(image_feature)[0]
 
-if predicted_label == 0:
-    predicted_label_name = 'cordana'
-elif predicted_label == 1:
-    predicted_label_name = 'healthy'
-elif predicted_label == 2:
-    predicted_label_name = 'pestalotiopsis'
-elif predicted_label == 3:
-    predicted_label_name = 'sigatoka'
-else:
-    predicted_label_name = 'HAYOLOH'
-
-print(f'Label asli: {label_name}')
-print(f'Label diprediksi: {predicted_label_name}')
-
-image = cv2.imread(PATH)
+# tampilkan gambar
+image = cv2.imread(filename)
 image = cv2.resize(image, (500, 500))
-THICKNESS = 2
-cv2.putText(image, f'Label asli: {label_name}',
-            (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 255, THICKNESS)
-cv2.putText(image, f'Label diprediksi: {predicted_label_name}',
-            (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, 255, THICKNESS)
+
+if label_name in predicted_label_name.values():
+    cv2.putText(image, f'Label asli: {label_name}',
+                (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, THICKNESS)
+    print(f'Label asli: {label_name}')
+
+
+cv2.putText(image, f'Label diprediksi: {predicted_label_name[predicted_label]}',
+            (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, THICKNESS)
 cv2.imshow('image', image)
-cv2.waitKey(50)
+cv2.waitKey(50000)
 cv2.destroyAllWindows()
+
+print(f'Label diprediksi: {predicted_label_name[predicted_label]}')
